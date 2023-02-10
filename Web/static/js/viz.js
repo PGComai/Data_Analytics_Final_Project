2022afca21468caa0802e62124ee9849603fbedd
PGComai <comai.alex@gmail.com>
2023-02-09 18:16:22 -0800

    finally got d3 working

---
+++ Web/static/js/viz.js
@@ -0,0 +1,13 @@
+function init() {
+    d3.csv("/static/js/BC_Final_clean_again.csv",function(data){
+        console.log(data);
+    })
+};
+
+init();
+
+function hist2d(isbn) {
+    d3.csv("/static/js/BC_Final_clean_again.csv",function(data){
+        
+    })
+}
