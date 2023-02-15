04530710db927348482f3057fd5b4aff199de668
PGComai <comai.alex@gmail.com>
2023-02-14 19:40:29 -0800

    ugh

--- Web/static/js/viz.js
+++ Web/static/js/viz.js
@@ -1,13 +1,32 @@
 function init() {
+
+    var selector = d3.select("#pick-book")
+
     d3.csv("/static/js/BC_Final_clean_again.csv",function(data){
-        console.log(data);
+        //console.log(data);
+        
+        var ids = Array.from(data.User_ID);
+
+        console.log(ids)
+
+        // ids.forEach((id) => {
+        //     selector
+        //         .append("option")
+        //         .text(id)
+        //         .property("value", id);
+        // });
     })
 };
 
 init();
 
-function hist2d(isbn) {
+function optionChanged(selection) {
+
+}
+
+function hist2d(selection) {
     d3.csv("/static/js/BC_Final_clean_again.csv",function(data){
+        
         
     })
 }
